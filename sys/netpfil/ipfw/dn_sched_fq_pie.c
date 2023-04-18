545c4d40ae8a372d6858da34777d2a518902ce81
deolsatish <52904167+deolsatish@users.noreply.github.com>
2023-04-19 08:04:07 +1000

    Test classification of packets into ECN and Non-ECN flows

--- sys/netpfil/ipfw/dn_sched_fq_pie.c
+++ sys/netpfil/ipfw/dn_sched_fq_pie.c
@@ -927,17 +927,17 @@ fq_pie_enqueue(struct dn_sch_inst *_si, struct dn_queue *_q,
 	
 
 	 /* classify a packet to queue number*/
-	idx = fq_pie_classify_flow(m, param->flows_cnt, si);
+	idx = fq_pie_classify_flow(m, param->flows_cnt/2, si);
 
-	printf("ECN Packet marked ? %d \n",ecn_mark(m));	
-	// if(ecn_mark(m))
-    // {
-    //     idx=idx+3;
-	// 	printf("ECN Packet flow index : %d \n",idx);
-    // }
-	// else{
-	// 	printf("NON-ECN Packet flow index : %d \n",idx);
-	// }
+	//printf("ECN Packet marked ? %d \n",ecn_mark(m));	
+	if(ecn_mark(m))
+    {
+        idx=idx+3;
+		printf("ECN Packet flow index : %d \n",idx);
+    }
+	else{
+		printf("NON-ECN Packet flow index : %d \n",idx);
+	}
 
 	/* enqueue packet into appropriate queue using PIE AQM.
 	 * Note: 'pie_enqueue' function returns 1 only when it unable to 
