ef1644c35f4b39de8f09c0e4cd09e4a45b531c4c
deolsatish <52904167+deolsatish@users.noreply.github.com>
2023-04-19 11:44:42 +1000

    Update dn_sched_fq_pie.c

--- sys/netpfil/ipfw/dn_sched_fq_pie.c
+++ sys/netpfil/ipfw/dn_sched_fq_pie.c
@@ -919,7 +919,7 @@ fq_pie_enqueue(struct dn_sch_inst *_si, struct dn_queue *_q,
 	 /* classify a packet to queue number*/
 	idx = fq_pie_classify_flow(m, param->flows_cnt/2, si);
 
-	printf("Is Packet ECN-Marked,%d \n",ecn_mark(m));	
+	//printf("Is Packet ECN-Marked,%d \n",ecn_mark(m));	
 	// if(ecn_mark(m)==1)
 	// {
 	// 	idx=idx+3;
