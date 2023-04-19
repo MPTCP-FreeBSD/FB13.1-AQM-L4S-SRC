7eddfa5c4263eebf51f6440b01b87749a200af76
deolsatish <52904167+deolsatish@users.noreply.github.com>
2023-04-19 14:09:07 +1000

    Update dn_sched_fq_pie.c

--- sys/netpfil/ipfw/dn_sched_fq_pie.c
+++ sys/netpfil/ipfw/dn_sched_fq_pie.c
@@ -942,19 +942,15 @@ fq_pie_enqueue(struct dn_sch_inst *_si, struct dn_queue *_q,
 		printf(" not-ECT \n");	/* not-ECT */
 	if ((ip->ip_tos & IPTOS_ECN_MASK) == IPTOS_ECN_CE)
 		printf("already marked \n");	/* already marked */
-	if((ip->ip_tos & IPTOS_ECN_MASK) != IPTOS_ECN_NOTECT)
-	{
-		/*
+
+	/*
 		* ecn-capable but not marked,
 		* mark CE and update checksum
 		*/
-		old = *(uint16_t *)ip;
-		ip->ip_tos |= IPTOS_ECN_CE;
-		ip->ip_sum = cksum_adjust(ip->ip_sum, old, *(uint16_t *)ip);
-		printf("ecn-capable but not marked mark CE and update checksum \n");
-	}
-
-	
+	old = *(uint16_t *)ip;
+	ip->ip_tos |= IPTOS_ECN_CE;
+	ip->ip_sum = cksum_adjust(ip->ip_sum, old, *(uint16_t *)ip);
+	printf("ecn-capable but not marked mark CE and update checksum \n");
 	
 
 
